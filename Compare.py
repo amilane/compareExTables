bf490446d01eaf5b649e53264c5b45e660940b21
AlinaSuchkova <amilane92@gmail.com>
2018-09-28 14:47:24 +0300

    доделано
    Нет конкатенации накладных, недостающие детали вписываются в overall из lisap

--- Compare.py
+++ Compare.py
@@ -3,7 +3,7 @@ import os
 from operator import itemgetter
 from itertools import groupby
 
-os.chdir('D:\\CompareTables')
+os.chdir('D:\\GitHub\\compareExTables')
 # ИМЯ ФАЙЛА
 filename = 'comparison  09 2018'
 
@@ -13,6 +13,7 @@ wb = openpyxl.load_workbook(filename + '.xlsx')
 overSheet = wb.get_sheet_by_name('overall list of details')
 lisapSheet = wb.get_sheet_by_name('LISAP')
 resultSheet = wb.create_sheet('Result')
+addToOverall = wb.create_sheet('Add To Overall')
 notInOverSheet = wb.create_sheet('Not in Overall')
 
 # ДИАПАЗОНЫ В ЛИСТАХ
@@ -20,41 +21,26 @@ overRange = range(4, 11285)
 lisapRange = range(2, 70081)
 
 # сбор данных для сравнения
+# колонка О в overall, колонки С и I в LISAP
 overID = [overSheet.cell(row = i, column = 15).value for i in overRange]
 lisID_NUM = [[lisapSheet.cell(row = i, column = 3).value, lisapSheet.cell(row = i, column = 9).value] for i in lisapRange if lisapSheet.cell(row = i, column = 3).value != "" and lisapSheet.cell(row = i, column = 9).value != ""]
 
-# уникальные значения
-uniqListID_NUM = []
-for i in lisID_NUM:
-  if i not in uniqListID_NUM:
-    uniqListID_NUM.append(i)
+lis_ID = [i[0] for i in lisID_NUM]
+lis_NUM = [i[1] for i in lisID_NUM]
 
-# сортировка-группировка по ID
-sortedUniqListID_NUM = sorted(uniqListID_NUM, key = lambda i: i[0])
-groupID_NUM = [[x for x in g] for k,g in groupby(sortedUniqListID_NUM, lambda i: i[0])]
-
-# схлопывание номеров накладных в 1 строку
-flatGroupID_NUM = []
-for group in groupID_NUM:
-	a = []
-	a.append(group[0][0])
-	b = ""
-	for g in group:
-		b += g[1] + ", "
-	a.append(b)
-	flatGroupID_NUM.append(a)
-
-# список накладных в порядке ID из overall
-lisID_in = ['' for i in overID]
-lisID_out = []
-for i in flatGroupID_NUM:
-	if i[0] in overID:
-		ind = overID.index(i[0])
-		lisID_in[ind] = i[1]
+lisID_in = []
+for i in overID:
+	if i in lis_ID:
+		ind = lis_ID.index(i)
+		lisID_in.append(lis_NUM[ind])
+		lis_ID.pop(ind)
+		lis_NUM.pop(ind)
 	else:
-		lisID_out.append(i)
+		lisID_in.append('')
 
-# вывод данных и сохранение нового документа
+dop_ID_NUM = list(filter(lambda i: i[0] in overID, list(zip(lis_ID, lis_NUM))))
+lisID_out = list(filter(lambda i: i[0] not in overID, list(zip(lis_ID, lis_NUM))))
+
 for i, k in zip(overRange, lisID_in):
   resultSheet.cell(row = i, column = 21).value = k
 
@@ -62,7 +48,13 @@ for i in range(1, len(lisID_out)+1):
   notInOverSheet.cell(row = i, column = 1).value = lisID_out[i-1][0]
   notInOverSheet.cell(row = i, column = 2).value = lisID_out[i-1][1]
 
+for i in range(1, len(dop_ID_NUM)+1):
+  addToOverall.cell(row = i, column = 15).value = dop_ID_NUM[i-1][0]
+  addToOverall.cell(row = i, column = 21).value = dop_ID_NUM[i-1][1]
 
 wb.save(filename + '_2.xlsx')
 
 print("OK")
+
+
+
